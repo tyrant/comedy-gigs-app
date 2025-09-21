9602ab8d703a0c82977f86dbc2bc71a644e62554
Mikey Clarke <pineutrino@gmail.com>
2025-09-21 18:00:51 +1200

    production.key copying added

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,7 +49,7 @@ namespace :deploy do
   namespace :check do
     before :linked_files, :set_production_key do
       on roles(:app), in: :sequence, wait: 10 do
-        %w[master].each do |file|
+        %w[master credentials/production.key].each do |file|
           unless test("[ -f #{shared_path}/config/#{file}.key ]")
             upload! "config/#{file}.key", "#{shared_path}/config/#{file}.key"
           end
