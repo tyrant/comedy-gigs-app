29d505bae4a73d7a56f33f6b156a0c52a9aad9a0
Mikey Clarke <pineutrino@gmail.com>
2025-09-27 14:26:48 +1200

    Losing /gigs base URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,4 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-
-  # Let's attempt hosting this at mikeyclarke.co.nz/gigs
-  config.relative_url_root = "/gigs"
 end
